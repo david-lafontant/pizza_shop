90a8b564ba7ccc1dce2f4a6006a156599e766575
david-lafontant <lafontant.david@gmail.com>
2023-01-08 00:20:50 -0600

    Add style to Home component

--- src/contexts/home/home.jsx
+++ src/contexts/home/home.jsx
@@ -4,11 +4,20 @@ import { useSelector } from 'react-redux';
 
 const Home = () => {
   const pizzas = useSelector((store) => store.home.pizzaItems);
+  const myStyle = {
+    display: 'grid',
+    gridTemplateColumns: 'repeat(3 ,200px)',
+    gridTemplateRows: 'auto',
+    gap: '2rem'
+  }
 
   return (
-    <div>Home
-    {pizzas.length > 0? pizzas.map((pizza)=> <Pizza key={pizza.id} pizza={pizza} />): <h2>No Pizza for today</h2>}
-  </div>
+    <section>
+      <div style={myStyle}>
+        {pizzas.length > 0 ? pizzas.map((pizza) => <Pizza key={pizza.id} pizza={pizza} />) : <h2>No Pizza for today</h2>}
+      </div>
+    </section>
+
   )
 }
 
