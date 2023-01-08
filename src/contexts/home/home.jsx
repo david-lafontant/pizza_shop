ba042e6cb7f8f2b2f10a2e0d939698f781c961e1
david-lafontant <lafontant.david@gmail.com>
2023-01-07 23:46:47 -0600

    Use useSelector to import pizzaItems in home and pass it as props to Pizza component

--- src/contexts/home/home.jsx
+++ src/contexts/home/home.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import PIzza from '../../components/pizza/PIzza';
+import Pizza from '../../components/pizza/PIzza';
+import { useSelector } from 'react-redux';
 
 const Home = () => {
+  const pizzas = useSelector((store) => store.home.pizzaItems);
+
   return (
     <div>Home
-    <PIzza />
-    </div>
+    {pizzas.length > 0? pizzas.map((pizza)=> <Pizza key={pizza.id} pizza={pizza} />): <h2>No Pizza for today</h2>}
+  </div>
   )
 }
 
