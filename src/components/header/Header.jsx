c1b130a15e21c4927ff2cbab1c5262bbbeb4359b
david-lafontant <lafontant.david@gmail.com>
2023-01-08 00:55:05 -0600

    Add navigation to header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useSelector} from 'react-redux';
-
+import { Link } from 'react-router-dom';
 const Header = () => {
   const amount = useSelector((store) => store.home.amount)
   const myStyle = {
@@ -10,10 +10,28 @@ const Header = () => {
     color: '#fff',
     padding: '1.1rem',
   }
+  
+  const navStyle = {
+    display: 'flex',
+    gap: '1.5rem',
+    listStyleType: 'none',
+  }
+
   return (
     <header style={myStyle}>
       <h2 style={{ textAlign: 'center', fontSize: '2rem'}}>Welcome to my Pizza Shop</h2>
       <p style={{ textAlign: 'center', fontSize: '1.5rem'}}>Your cart contains <span>{amount}</span> Pizzas</p>
+    <nav>
+      <ul style={navStyle}>
+        <li>
+          <Link to='/'>Home</Link>
+        </li>
+        <li>
+          <Link to='/cart'>Cart</Link>
+        </li>
+      </ul>
+    </nav>
+    
     </header>
   )
 }
