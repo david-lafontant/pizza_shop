378a5c57a84776e570d3be44f032deb1a687f3e9
david-lafontant <lafontant.david@gmail.com>
2023-01-07 22:31:22 -0600

    Import Pizza component in Home

--- src/contexts/home/home.jsx
+++ src/contexts/home/home.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import PIzza from '../../components/pizza/PIzza';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>Home
+    <PIzza />
+    </div>
   )
 }
 
