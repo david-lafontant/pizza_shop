6a220b581c3bbf1d34c14213d31bda06cbbf2ea1
david-lafontant <lafontant.david@gmail.com>
2023-01-07 22:00:49 -0600

    Add header tag and style Header component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
 
 const Header = () => {
+  const myStyle = {
+    width: '100%',
+    height: '10vh',
+    backgroundColor: '#b22222',
+    color: '#fff',
+    padding: '1.1rem',
+  }
   return (
-    <div>Header</div>
+    <header style={myStyle}>
+      <h2 style={{ textAlign: 'center', fontSize: '2rem'}}>Welcome to my Pizza Shop</h2>
+    </header>
   )
 }
 
