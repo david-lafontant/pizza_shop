9bfe407cb66f6f660e0514c4e6a64b1439e75c56
david-lafontant <lafontant.david@gmail.com>
2023-01-07 23:45:17 -0600

    Display quantity in header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import React from 'react';
+import { useSelector} from 'react-redux';
 
 const Header = () => {
+  const amount = useSelector((store) => store.home.amount)
   const myStyle = {
     width: '100%',
     height: '10vh',
@@ -11,6 +13,7 @@ const Header = () => {
   return (
     <header style={myStyle}>
       <h2 style={{ textAlign: 'center', fontSize: '2rem'}}>Welcome to my Pizza Shop</h2>
+      <p style={{ textAlign: 'center', fontSize: '1.5rem'}}>Your cart contains <span>{amount}</span> Pizzas</p>
     </header>
   )
 }
