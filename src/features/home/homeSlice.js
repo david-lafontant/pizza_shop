256e3f4a5934663628288689086e41f4687ff145
david-lafontant <lafontant.david@gmail.com>
2023-01-09 06:48:35 -0600

    Create addToCart action

--- src/features/home/homeSlice.js
+++ src/features/home/homeSlice.js
@@ -3,15 +3,23 @@ import pizzas from "../../assets/data/pizzas";
 
 const initialState = {
   pizzaItems: pizzas,
-  amount: 0,
+  quantity: 0,
   isLoading: true,
 }
 
-
 const homeSlice = createSlice({
   name: 'home',
-  initialState
+  initialState,
+
+  reducers: {
+    addToCart: (state, { payload }) => {
+      const pizzaItem = state.pizzaItems.find((item) => item.id === payload);
+      pizzaItem.isIncart = true;
+    }
+  }
+
 })
 
 // console.log(homeSlice.reducer);
+export const { addToCart } = homeSlice.actions;
 export default homeSlice.reducer;
