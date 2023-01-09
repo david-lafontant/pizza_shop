8b8af731033821f103afaed2066c43b07823ffd5
david-lafontant <lafontant.david@gmail.com>
2023-01-09 06:51:40 -0600

    Display pizza that have isInCart as true in Cart

--- src/contexts/cart/cart.jsx
+++ src/contexts/cart/cart.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import Pizza from '../../components/pizza/Pizza';
 
 const Cart = () => {
+  const cartPizzas = useSelector((store) => store.home.pizzaItems);
+  const list = [];
+
+  if(cartPizzas.length > 0){
+    cartPizzas.forEach(item => {
+      if(item.isIncart === true){
+        list.push(item);
+      }
+    })
+  }
+
+  const myStyle = {
+    display: 'grid',
+    gridTemplateColumns: 'repeat(3 ,200px)',
+    gridTemplateRows: 'auto',
+    gap: '2rem'
+  };
+
   return (
-    <div>Cart</div>
-  )
+    <section>
+      <div style={myStyle}>
+        {list.map((pizza) => <Pizza key={pizza.id} pizza={pizza} />)}
+      </div>
+    </section>);
 }
 
 export default Cart;
