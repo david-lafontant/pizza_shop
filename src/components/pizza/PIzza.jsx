27c05a890afa87038c2452864c422223d02241a9
david-lafontant <lafontant.david@gmail.com>
2023-01-09 06:50:50 -0600

    Update Pizza component to display carted pizza

--- src/components/pizza/PIzza.jsx
+++ src/components/pizza/PIzza.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { addToCart } from '../../features/home/homeSlice';
 
-const PIzza = (props) => {
+const Pizza = (props) => {
+  const dispatch = useDispatch()
 
   const cardStyle = {border: 'thin solid black', display: 'flex', flexDirection: 'column', gap: '1rem', padding:'1rem', width: 'max-content', boxShadow: 'rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px'}
   
-
-
 
   return (
     <article style={cardStyle}>
@@ -14,18 +15,21 @@ const PIzza = (props) => {
     <img src={props.pizza.image} alt={props.pizza.name} style={{width: '100px', height: '100px'}} />
     <p>{props.pizza.name}</p>
     <p>{props.pizza.price}</p>
-    <button type="button" onClick={()=>console.log(props.pizza.id)}>Add to cart</button>
+    {!props.pizza.isIncart?  <button type="button" onClick={()=>dispatch(addToCart(props.pizza.id))}>Add to cart</button> :  <button type="button" onClick={()=>console.log(props.pizza.id)}>Remove to Cart</button>}
+   
+   
     </article>
   )
 }
 
-PIzza.propTypes = {
+Pizza.propTypes = {
   pizza: PropTypes.shape({
     id: PropTypes.number,
     price: PropTypes.number,
     image: PropTypes.string,
     name: PropTypes.string,
+    isInCart: PropTypes.bool,
   })
 }
 
-export default PIzza
+export default Pizza;
