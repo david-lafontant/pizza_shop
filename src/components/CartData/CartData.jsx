f0797911c62e6b1423611abbed9cf4d36fa6057c
david-lafontant <lafontant.david@gmail.com>
2023-01-12 06:11:45 -0600

    Update CartData component - display info for a 'carted' pizza

--- src/components/CartData/CartData.jsx
+++ src/components/CartData/CartData.jsx
@@ -1,13 +1,70 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { PlusIcon, TrashIcon } from '../icons/Icons';
+import { removeFromCart, incrementQuantity, decrementQuantity } from '../../features/home/homeSlice'
+// import { PlusIcon, MinusIcon, TrashIcon } from '../icons/Icons'
 
 
-const CartData = props => {
+
+const CartData = ({ pizza }) => {
+  const dispatch = useDispatch();
+
+  const myButton = {
+    width: '25px',
+    height: '25px',
+    border: '1px solid #999999',
+    borderRadius: '50%'
+  }
+
   return (
-    <div>CartData</div>
-  )
+    
+     <div className="card">
+    <div className="card-header">
+      <img src={pizza.image} alt="pizza.name" />
+    </div>
+    <div className="card-body">
+      <span className="tag tag-purple">{pizza.price}</span>
+      <h4>
+        {pizza.name}
+      </h4>
+      <button className="cart-btn" type="button" 
+      
+      // removeFromCart
+      // onClick={dispatch(()=> removeFromCart(pizza.id))
+        onClick={()=>dispatch(removeFromCart(pizza.id))
+      
+      
+      }>
+        <TrashIcon />
+      </button>
+      <div className="user">
+      {/* onClick={()=>dispatch(addToCart(props.pizza.id))} */}
+        <div className="user-info">
+        <button className="cart-btn" type="button" onClick={dispatch(()=> incrementQuantity(pizza.id))}>
+          <PlusIcon />
+        </button>
+          <h5>{pizza.quantity}</h5>
+          <button className="cart-btn" type="button" onClick={dispatch(()=> decrementQuantity(pizza.id))}>
+          <PlusIcon />
+        </button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+  );
 }
 
-CartData.propTypes = {}
+CartData.propTypes = {
+  pizza: PropTypes.shape({
+    id: PropTypes.number,
+    price: PropTypes.number,
+    image: PropTypes.string,
+    name: PropTypes.string,
+    isInCart: PropTypes.bool,
+  })
+}
 
 export default CartData
