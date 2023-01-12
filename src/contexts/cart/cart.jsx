be3c871317168980e25df6013eb79ba915cedcad
david-lafontant <lafontant.david@gmail.com>
2023-01-12 06:13:38 -0600

    Display 'carted' pizza

--- src/contexts/cart/cart.jsx
+++ src/contexts/cart/cart.jsx
@@ -1,30 +1,36 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import Pizza from '../../components/pizza/Pizza';
+import CartData from '../../components/CartData/CartData';
 
 const Cart = () => {
   const cartPizzas = useSelector((store) => store.home.pizzaItems);
   const list = [];
 
-  if(cartPizzas.length > 0){
+  if (cartPizzas.length > 0) {
     cartPizzas.forEach(item => {
-      if(item.isInCart === true){
+      if (item.isInCart === true) {
         list.push(item);
       }
     })
   }
 
   const myStyle = {
-    display: 'grid',
-    gridTemplateColumns: 'repeat(3 ,200px)',
-    gridTemplateRows: 'auto',
-    gap: '2rem'
+    display: 'flex',
+    flexDirection: 'column',
+    gap: '2rem',
+    padding: '2rem'
   };
+
+  const cart_card = {
+    display: 'flex',
+    width: 'max-content',
+  }
 
   return (
     <section>
       <div style={myStyle}>
-        {list.map((pizza) => <Pizza key={pizza.id} pizza={pizza} />)}
+      {list.map(pizza => <CartData key={pizza.name} pizza={pizza} /> )}
+       
       </div>
     </section>);
 }
