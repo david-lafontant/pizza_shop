802e59c1bd660238da5335ceba27110a9e8ff2a9
david-lafontant <lafontant.david@gmail.com>
2023-01-12 09:06:41 -0600

    Implement granTotal calculation

--- src/features/home/homeSlice.js
+++ src/features/home/homeSlice.js
@@ -8,6 +8,16 @@ const initialState = {
   isLoading: true,
 }
 
+const calcGranTotal = (state) => {
+  state.grandTotal = 0;
+  state.pizzaItems.forEach((item)=>{   
+  state.grandTotal += item.total
+  })
+}
+
+
+
+
 const homeSlice = createSlice({
   name: 'home',
   initialState,
@@ -16,32 +26,47 @@ const homeSlice = createSlice({
     addToCart: (state, { payload }) => {
       const pizzaItem = state.pizzaItems.find((item) => item.id === payload);
       pizzaItem.isInCart = true;
-      pizzaItem.quantity = 1
+      pizzaItem.quantity = 1;
+      pizzaItem.total = pizzaItem.price;
       state.number += 1;
+      calcGranTotal(state)
+
     },
     removeFromCart: (state, {payload}) =>{
       const pizzaItem = state.pizzaItems.find((item) => item.id === payload);
       pizzaItem.isInCart = false;
-      pizzaItem.quantity = 0
+      pizzaItem.quantity = 0;
+      pizzaItem.total = 0;
       state.number -= 1;
+      calcGranTotal(state)
     },
     incrementQuantity: (state, { payload }) => {
       const pizzaItem = state.pizzaItems.find((item) => item.id === payload);
       pizzaItem.quantity += 1;
+      pizzaItem.total = pizzaItem.quantity*pizzaItem.price
       state.number += 1;
+      calcGranTotal(state)
     },
     decrementQuantity: (state, { payload }) => {
       const pizzaItem = state.pizzaItems.find((item) => item.id === payload);
-      if(pizzaItem.quantity>0 )pizzaItem.quantity -= 1;
+      if(pizzaItem.quantity>0 ){
+
+      pizzaItem.quantity -= 1;
+      pizzaItem.total = pizzaItem.quantity*pizzaItem.price
       state.number -= 1;
+      calcGranTotal(state)}
     },
     removeAllFromCart: (state) => {
       state.pizzaItems.forEach((item)=> {
         item.isInCart = false;
         item.quantity = 0;
+        pizzaItem.quantity = 0;
       })
       state.number = 0;
-    }
+      calcGranTotal(state)
+    
+    },
+    
 
   }
 
